650c1e44e73e1f029fbb7d022825b89a6b1e83a9
Tatiana Stewart <tstwrt7@gmail.com>
2025-04-17 19:34:06 -0400

    Add Home page component and corresponding CSS styling

---
+++ client/react/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Home.css";
+
+export default function Home() {
+
+  return (
+      <div className="home-container">
+        <h1>Welcome to OBX!</h1>
+        <p>We are here to help you find the best oils for your needs.</p>
+      </div>
+  );
+}
